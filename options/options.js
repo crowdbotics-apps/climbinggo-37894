f8a2c5f4018b64070f27be1dfee9c4d70185397f
Crowdbotics <team@crowdbotics.com>
2022-12-14 13:55:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "climbinggo_37894",
+  url: "https://climbinggo_37894.botics.co",
+  api: "https://climbinggo_37894.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
